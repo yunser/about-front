d3bc84f62f9f7eb776baef99ea17a9c50cdf0ee9
cjh <1418503647@qq.com>
2018-05-01 17:20:29 +0800

    update api domain to https

--- src/config/index.js
+++ src/config/index.js
@@ -4,10 +4,10 @@ let imgDomain
 let apiDomain
 if (process.env.NODE_ENV === 'production') {
     imgDomain = 'http://120.79.29.47'
-    apiDomain = 'http://node.api.yunser.com'
+    apiDomain = 'https://nodeapi.yunser.com'
 } else {
     imgDomain = 'http://120.79.29.47'
-    apiDomain = 'http://node.api.yunser.com'
+    apiDomain = 'https://nodeapi.yunser.com'
 }
 
 module.exports = {
